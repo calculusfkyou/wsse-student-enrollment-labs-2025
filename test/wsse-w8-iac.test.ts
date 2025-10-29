a3e420b754bc912d2504c7a12f5022a53a47d560
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2025-10-29 16:52:36 +0800

    Add files via upload

---
+++ test/wsse-w8-iac.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WsseW8Iac from '../lib/wsse-w8-iac-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wsse-w8-iac-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WsseW8Iac.WsseW8IacStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
