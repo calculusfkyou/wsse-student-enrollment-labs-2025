157bc20dd82d7f3c902e7fdb7df6c7bfd99990b4
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2025-11-05 13:43:34 +0800

    Update wsse-w8-iac-stack.ts

--- lib/wsse-w8-iac-stack.ts
+++ lib/wsse-w8-iac-stack.ts
@@ -1,5 +1,6 @@
 // lib/wsse-w8-iac-stack.ts (CloudShell 適用版本)
 
+import * as logs from 'aws-cdk-lib/aws-logs';
 import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib'; // <-- 確保有這一行
 import { Construct } from 'constructs';
